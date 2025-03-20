1d6cd501c2d8ddc3af0276566534f2b4fffb74bb
Ayush Sinha <149171873+ayushsinhafr@users.noreply.github.com>
2025-03-20 22:37:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ document.getElementById('processButton').addEventListener('click', function () {
     document.getElementById('uploadStatus').textContent = '';
     document.getElementById('uploadStatus').classList.remove('show');
 
-    fetch("https://bgremove-9dab4bc42a85.herokuapp.com", {
+    fetch("https://bgremove-9dab4bc42a85.herokuapp.com/bg-remove", {
         method: "POST",
         body: formData
     })
